63d1c5680770db90cd74e1aafdcc0677e0976c19
Mnabawy <m_ninja61@yahoo.com>
2021-04-03 17:06:50 +0200

    authenticate new users

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  public form = {
+    name: null,
+    email: null,
+    password: null,
+    password_confirmation: null,
+  };
 
-  constructor() { }
+  public error:any = [];
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    return this.http
+      .post('http://localhost:8000/api/signup', this.form)
+      .subscribe(
+        (data) => console.log(data),
+        (error) => this.handleError(error)
+      );
   }
 
+  handleError(error:any) {
+    this.error = error.error.errors;
+    console.log(this.error);
+  }
+
+  ngOnInit(): void {}
 }
